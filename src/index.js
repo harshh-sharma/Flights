3ee847c840dafde6e5cc252f1b895e3b2f08af75
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2024-12-31 21:54:13 +0530

    added flights model with foreign key constraints

--- src/index.js
+++ src/index.js
@@ -9,20 +9,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api',apiRouter);
 
-app.listen(PORT,async () => {
+app.listen(PORT,() => {
     console.log(`server successfully running on ${PORT}`);
-    const {City,Airport} = require('./models');
-
-    // const indore = await City.create({name:"indore"});
-    // console.log(indore);
-    // const rajabhojAirport = await indore.createAirport({name:"Rajabhoj",code:"Bpl"})
-    // console.log(rajabhojAirport);
-    const gwalior = await City.create({name:'Gwalior'});
-    console.log(gwalior);
-    const airportGwalior = await gwalior.createAirport({name:'Gwalior airport',code:'Gwl'})
-    console.log(airportGwalior);
-    
-    
-    
-    
+ 
 })
